ae024ef9b2ccd8615f3377994cb12958e96abdf6
Kanit Wongsuphasawat <kanitw@gmail.com>
2012-11-25 22:17:58 -0800

    Fix bug introduced from backbone.

--- js/app.stacked.chart.js
+++ js/app.stacked.chart.js
@@ -87,9 +87,16 @@
   app.ReviewDir = Backbone.Collection.extend(
     (function(){ //use anonymous function here so we can have private variable for this class
       var total = {};
+      var that;
       return  {
         model: window.app.Review,
         initialize: function() {
+          that = this;
+          // console.log("ReviewDir.Models");
+          // console.log(this);
+          // that.initPos();  -- should be called here but a bug theat cause this.models to be null prevent us from calling it here!!
+        },
+        initPos: function(){
           var reviewData = _(this.models).pluck('attributes');
           reviewData.forEach(function(d) {
             if(!total[d.score]) {
