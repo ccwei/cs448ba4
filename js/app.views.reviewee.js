84982e08caf74e9852a88b78461165afa2fce8c7
Kanit Wongsuphasawat <kanitw@gmail.com>
2012-12-03 21:20:01 -0800

    Fix merge bugs

--- js/app.views.reviewee.js
+++ js/app.views.reviewee.js
@@ -21,11 +21,17 @@
       var that = this;
       this.viewId = total ++;
       this.$el.html(this.template(this));
+
       this.feedbacksAggregatedView = new app.FeedbacksAggregatedView({el: this.$el.find("#ind-tab-aggregate-grid-"+this.viewId)
       });
       this.revieweeDetailView =  new app.RevieweeDetailView({
         el: this.$el.find("#ind-reviewee-detail-"+this.viewId)
       });
+      this.feedbacksView = new app.FeedbacksView({
+        el: $('#ind-tab-individual-review-'+this.viewId)
+        // id: '#ind-tab-individual-review'
+      });
+
       this.createTagCloud('ind-tab-tag-cloud');
       $('a[data-toggle="tab"]').on('shown', function (e) {
         if($(e.target).attr('href') === '#ind-tab-tag-cloud') {
@@ -36,20 +42,17 @@
 
     },
     loadData: function(reviewee){
+      this.feedbacksAggregatedView.loadData(reviewee.reviews);
+      this.revieweeDetailView.loadData(reviewee);
+      this.feedbacksView.loadData(reviewee.reviews);
+
       var scores = _.map(reviewee.reviews, function(d, idx) {
         return {score: d.score};
       });
       // If you decide not to parse the whole d.reviews object, I would suggest your to use
       // var scores = _(d.reviews).pluck("score");
 
-      //TODO(kanitw): all these initialization should be in initialize and all these classes should have loadData so we can reuse views
-      this.revieweeDetailView =  new app.RevieweeDetailView(reviewee);
-      this.indScoreView = new app.IndScoreView(reviewee);
-      this.feedbacksView = new app.FeedbacksView({
-        collection:reviewee.reviews,
-        el: $('#ind-tab-individual-review')
-        // id: '#ind-tab-individual-review'
-      });
+
       this.indScoresView = new app.IndScoresView({
         collection:scores,
         el: $('#indscores')
@@ -85,7 +88,7 @@
     render: function(){
       //DO NOTHING since we haven't used the template for this view yet
       //TODO: not a bad idea to use template here too!
-      
+
     }
   });
 
