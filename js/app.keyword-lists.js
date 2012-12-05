2c122abb973ad31e788ab32fcbf39e60acff04a5
Chih-Chiang Wei <chihchiang.wei@gmail.com>
2012-12-05 01:29:09 -0800

    add bar in keyword list, fix on click

--- js/app.keyword-lists.js
+++ js/app.keyword-lists.js
@@ -33,32 +33,40 @@
       this.$el.find(".search-field").on('keyup',onSearchTextChange);
       this.render();
     },
+    setliOnclick: function (liOnclick) {
+      var that = this;
+      this.liOnclick = liOnclick;
+      this.$el.delegate('li', 'click', function(event) {
+            that.liOnclick(event);
+        });
+    },
     render: function () {
       var that = this;
 
       this.keywordLists = {};
 
-      this.$el.html(this.template())
-        .delegate('li', 'click', function(event) {
-            that.liOnclick(event);
-        });
-
       _(app.FEEDBACK_TYPE).each(function(type){
         $(that.$el.selector + " .keyword-list-" + type + ' ol').children().remove();
       });
       var matchCount = 0;
+      var maxCount = {};
       _(app.FEEDBACK_TYPE).each(function (type) {
         _(that.frequentWords[type].feedbackWords).each(function (d) {
           //d is like ["nice", {count:17, reviews: array of review}]
           if(that.keyword.length === 0 || d[0].match(new RegExp(that.keyword, "i"))){
-            var li = $('<li/>').append(d[0]);
-            li.addClass('clickable');
+            if(!maxCount[type] || maxCount[type] < d[1].count)
+              maxCount[type] = d[1].count;
+            var li = $('<li/>').append(d[0] + ' (' + d[1].count + ')');
+            li.addClass('clickable keyword');
             matchCount++;
             if(that.keyword.length > 0){
               console.log(li);
               li.highlight(that.keyword);
             }
-            $(that.$el.selector + " .keyword-list-" + type + ' ul').append(li);
+            var bar = $('<div/>').addClass('keyword-item-bar');
+            $(that.$el.selector + " .keyword-list-" + type + ' ul').append(bar).append(li);
+            var percentage = (d[1].count * 1.0) / maxCount[type];
+            bar.width(percentage * 80 + '%');
           }
         });
       });
