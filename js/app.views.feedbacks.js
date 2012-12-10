89eb73f2c4c12771bbb678c3d5e2ff52b19f9221
Kanit Wongsuphasawat <kanitw@gmail.com>
2012-12-09 23:03:17 -0800

    Left clickable

--- js/app.views.feedbacks.js
+++ js/app.views.feedbacks.js
@@ -16,6 +16,8 @@
     template: _.template($("#feedbacksTemplate").html()),
     item_template: _.template($("#feedbackGridTemplate").html()),
     score_template: _.template($("#indScoreTemplate").html()),
+    smallgrid_template: _.template($("#smallGridTemplate").html()),
+    scoreListItem_template: _.template($("#scoreListItem").html()),
     tagName: "div",
     className: "feedbacks-view",
 
@@ -35,6 +37,7 @@
 
       this.$el.html(this.template());
 
+      var byScore = {};
 
       _.each(this.collection.models, function (item) {
         // that.renderFeedback(item);
@@ -60,9 +63,17 @@
 
         //this.$el.find('.indscores')
         //
-        this.$el.find('.affix')
-          .append(this.score_template(item.toJSON()))
-          .append("<hr/>");
+        // this.$el.find('.affix')
+        //   .append(this.score_template(item.toJSON()))
+        //   .append("<hr/>");
+
+        var roundedScore = Math.round(item.get('score'));
+
+        if(byScore[roundedScore]){
+          byScore[roundedScore].push(item);
+        }else{
+          byScore[roundedScore] = [item];
+        }
 
 
         // this.$el.find(".indscores").append("Put your render of indscore view here!");
@@ -70,7 +81,32 @@
 
       }, this);
 
-      var $affix = this.$el.find('.affix');
+      var $indscores = this.$el.find('.indscores');
+
+      for(var i=10 ; i>0 ; i--){
+        if(byScore[i]){
+          var items = byScore[i];
+
+          var $group = $(this.scoreListItem_template({score:i}));
+          var $groupGrids = $group.find(".grids");
+
+          for(var j=0 ;j<items.length ; j++){
+            var $grid = $(this.smallgrid_template());
+
+            for(var t=0 ; t<4 ; t++){
+              var type = app.FEEDBACK_TYPE[t];
+              var opacity =Math.random()*0.8 + 0.2 ;
+              $grid.find("."+type).attr('style',"opacity:"+opacity+";");
+            }
+
+            $groupGrids.append($grid);
+          }
+          // var len = group;
+
+          $indscores.append($group);
+
+        }
+      }
 
 
       // _.each(this.collection.models, function (item) {
